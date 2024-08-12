ca2714e7bb391c77c0c30bdb592920f6949cac8d
kishore <kishore.cb20@bitsathy.acx.in>
2024-08-12 22:15:45 +0530

    linkedin

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
           <ul className="dropdown-menu">
             <li><a href="mailto:dalekishore002@gmail.com">Email</a></li>
             <li><a href="tel:+91 7845318136">Phone</a></li>
-            <li><a href="www.linkedin.com/in/kishore-r-ba3a56200" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li><a href="https://linkedin.com/in/kishore-r-ba3a56200" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
             <li><a href="https://github.com/Kisore06" target="_blank" rel="noopener noreferrer">Github</a></li>
           </ul>
         )}
